84b077965709f5aaf479fb7589ab876e2c07ecfd
coderabah <mmuomatito@gmail.com>
2022-10-27 07:34:23 -0700

    Update to putchar project

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,11 @@
 #include "main.h"
 
-/** main - Prints putchar
- * Return: Always 0
-*/
-
+/**
+ * main - Starting point
+ *
+ * Return: Always return 0
+ *
+ */
 int main(void)
 {
   _putchar("_");
@@ -15,6 +17,5 @@ int main(void)
   _putchar("a");
   _putchar("r");
   _putchar("\n");
-
-  return(0)
+  return(0);
 }
